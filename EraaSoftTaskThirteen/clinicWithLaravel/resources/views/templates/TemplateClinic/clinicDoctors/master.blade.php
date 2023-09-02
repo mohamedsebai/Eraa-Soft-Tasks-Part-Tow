29b8694ec40b8bfb0c3258ad2d54f759d557e682
Mohamed sebai <mohamedseabeai@gmail.com>
2023-09-02 05:58:37 -0700

    Update master.blade.php

--- EraaSoftTaskThirteen/clinicWithLaravel/resources/views/templates/TemplateClinic/clinicDoctors/master.blade.php
+++ EraaSoftTaskThirteen/clinicWithLaravel/resources/views/templates/TemplateClinic/clinicDoctors/master.blade.php
@@ -4,6 +4,6 @@
         @include('templates.TemplateClinic.clinicDoctors.nav')
 
 
-
+@yield('content')
 @include('templates.TemplateClinic.clinicDoctors.footer_content')
 @include('templates.TemplateClinic.clinicDoctors.footer')
